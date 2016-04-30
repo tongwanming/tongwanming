ba8b7e6f3ab5d290073c30ceafc5b81b1fb35277
tongwanming <359209162@qq.com>
2016-04-30 11:16:15 +0800

    gcd

--- gcd Demo/ViewController.m
+++ gcd Demo/ViewController.m
@@ -21,6 +21,32 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)gcd{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        
+    });
+    
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        
+    });
+    
+    
+    dispatch_sync(dispatch_get_main_queue(), ^{
+        
+    });
+    dispatch_sync(dispatch_get_main_queue(), ^{
+        
+    });
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 4*NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+        
+    });
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 5*NSEC_PER_SEC), dispatch_get_global_queue(0, 0), ^{
+        
+    });
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
